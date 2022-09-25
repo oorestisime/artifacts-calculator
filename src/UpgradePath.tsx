1cc77b023a4ef537317c22897c7ff2f24c4e0470
Orestis Ioannou <oorestisime@gmail.com>
2022-09-26 01:18:50 +0300

    Dont overflow

--- src/UpgradePath.tsx
+++ src/UpgradePath.tsx
@@ -61,123 +61,121 @@ export const UpgradePath = () => {
           />
           <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle">
-              <div className="overflow-hidden lg:overflow-auto scrollbar:!w-1.5 scrollbar:!h-1.5 scrollbar:bg-transparent scrollbar-track:!bg-sky-100 scrollbar-thumb:!rounded scrollbar-thumb:!bg-sky-300 scrollbar-track:!rounded dark:scrollbar-track:!bg-sky-500/[0.16] dark:scrollbar-thumb:!bg-sky-500/50 max-h-96 supports-scrollbars:pr-2 lg:max-h-96">
-                <table
-                  className="min-w-full border-separate"
-                  style={{ borderSpacing: 0 }}
-                >
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
-                      >
-                        Artifact
-                      </th>
+              <table
+                className="min-w-full border-separate"
+                style={{ borderSpacing: 0 }}
+              >
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                    >
+                      Artifact
+                    </th>
 
-                      <th
-                        scope="col"
-                        className="sticky top-0 z-10  border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter "
-                      >
-                        Level
-                      </th>
-                      <th
-                        scope="col"
-                        className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                      >
-                        Star
-                      </th>
-                      <th
-                        scope="col"
-                        className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pr-4 pl-3 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
-                      >
-                        Owned
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className=" bg-white">
-                    {Object.keys(artifactsMapNames)
-                      .filter((artifact) =>
-                        artifact
-                          .toLowerCase()
-                          .startsWith(nameFilter.toLowerCase())
-                      )
-                      .map((artifact) => (
-                        <tr key={artifact}>
-                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                            <div className="flex items-center">
-                              <div className="h-10 w-10 flex-shrink-0">
-                                <img
-                                  className="h-10 w-10 rounded-full"
-                                  src={artifactImages[artifact]}
-                                  alt=""
-                                />
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10  border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter "
+                    >
+                      Level
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    >
+                      Star
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pr-4 pl-3 backdrop-blur backdrop-filter sm:pr-6 lg:pr-8"
+                    >
+                      Owned
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className=" bg-white">
+                  {Object.keys(artifactsMapNames)
+                    .filter((artifact) =>
+                      artifact
+                        .toLowerCase()
+                        .startsWith(nameFilter.toLowerCase())
+                    )
+                    .map((artifact) => (
+                      <tr key={artifact}>
+                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                          <div className="flex items-center">
+                            <div className="h-10 w-10 flex-shrink-0">
+                              <img
+                                className="h-10 w-10 rounded-full"
+                                src={artifactImages[artifact]}
+                                alt=""
+                              />
+                            </div>
+                            <div className="ml-4">
+                              <div className="font-medium text-gray-900">
+                                {artifactsMapNames[artifact]}
                               </div>
-                              <div className="ml-4">
-                                <div className="font-medium text-gray-900">
-                                  {artifactsMapNames[artifact]}
-                                </div>
-                                <div className="text-gray-500 capitalize">
-                                  {artifactsMapRarity[artifact]}
-                                </div>
+                              <div className="text-gray-500 capitalize">
+                                {artifactsMapRarity[artifact]}
                               </div>
                             </div>
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            <ArtifactLevelSelect
-                              id={`${artifact}level`}
-                              value={currentSetup[artifact]?.level}
-                              onChange={(e) => {
-                                setCurrentSetup({
-                                  ...currentSetup,
-                                  [artifact]: {
-                                    ...currentSetup[artifact],
-                                    name: artifact,
-                                    level: +e.target.value,
-                                  },
-                                });
-                              }}
-                            />
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            <ArtifactStarSelect
-                              id={`${artifact}star`}
-                              value={currentSetup[artifact]?.star}
-                              onChange={(e) => {
-                                setCurrentSetup({
-                                  ...currentSetup,
-                                  [artifact]: {
-                                    ...currentSetup[artifact],
-                                    name: artifact,
-                                    star: +e.target.value,
-                                  },
-                                });
-                              }}
-                            />
-                          </td>
-                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                            <input
-                              id={`${artifact}enabled`}
-                              type="checkbox"
-                              checked={currentSetup[artifact]?.unlocked}
-                              onChange={(e) => {
-                                setCurrentSetup({
-                                  ...currentSetup,
-                                  [artifact]: {
-                                    ...currentSetup[artifact],
-                                    name: artifact,
-                                    unlocked: e.target.checked,
-                                  },
-                                });
-                              }}
-                              className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-500"
-                            />
-                          </td>
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
-              </div>
+                          </div>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <ArtifactLevelSelect
+                            id={`${artifact}level`}
+                            value={currentSetup[artifact]?.level}
+                            onChange={(e) => {
+                              setCurrentSetup({
+                                ...currentSetup,
+                                [artifact]: {
+                                  ...currentSetup[artifact],
+                                  name: artifact,
+                                  level: +e.target.value,
+                                },
+                              });
+                            }}
+                          />
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <ArtifactStarSelect
+                            id={`${artifact}star`}
+                            value={currentSetup[artifact]?.star}
+                            onChange={(e) => {
+                              setCurrentSetup({
+                                ...currentSetup,
+                                [artifact]: {
+                                  ...currentSetup[artifact],
+                                  name: artifact,
+                                  star: +e.target.value,
+                                },
+                              });
+                            }}
+                          />
+                        </td>
+                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                          <input
+                            id={`${artifact}enabled`}
+                            type="checkbox"
+                            checked={currentSetup[artifact]?.unlocked}
+                            onChange={(e) => {
+                              setCurrentSetup({
+                                ...currentSetup,
+                                [artifact]: {
+                                  ...currentSetup[artifact],
+                                  name: artifact,
+                                  unlocked: e.target.checked,
+                                },
+                              });
+                            }}
+                            className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-500"
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
