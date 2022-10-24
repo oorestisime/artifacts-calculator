f6e0f308b334fe2535eb4017f2f77aa6b269d889
Konstantinos Angeletopoulos <kostasAngels@outlook.com>
2022-10-24 10:53:33 +0300

    Fixed value on the dropdown list of ArtificactStarSelect

--- src/ArtifactStarSelect.tsx
+++ src/ArtifactStarSelect.tsx
@@ -27,7 +27,7 @@ export const ArtifactStarSelect = ({
         name={name || id}
         className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
       >
-        <option value="1">0</option>
+        <option value="0">0</option>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
